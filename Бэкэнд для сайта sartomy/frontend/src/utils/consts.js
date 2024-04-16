7e8ed917ada3dbc380b95a4fd0fcd9c05b93ba25
haron2901 <143057166+haron2901@users.noreply.github.com>
2024-04-17 00:42:06 +0300

    Delete Бэкэнд для сайта sartomy directory

--- Бэкэнд для сайта sartomy/frontend/src/utils/consts.js
+++
@@ -1,7 +0,0 @@
-export const TABLET_WIDTH = 767
-export const PHONE_WIDTH = 480
-
-export const PRODUCTS_ROUTE = '/products'
-export const CREATE_CUSTOM_ROUTE = '/createCustom'
-export const PROJECTS_ROUTE = '/projects'
-export const QUICK_TOOL_ROUTE = '/quickTool'
